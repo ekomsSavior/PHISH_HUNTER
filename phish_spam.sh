c3920c3c2fc349465685d4577ef3142f313287d9
ekomsSavior <161806886+ekomsSavior@users.noreply.github.com>
2025-04-19 12:03:13 -0400

    Create phish_spam.sh

---
+++ phish_spam.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# phish_spam.sh
+# by ekomsSavior
+
+TARGET_URL="http://74.81.94.123/"                  #edit for you scammers ip
+
+while true; do
+  USER="user$(openssl rand -hex 3)"
+  PASS="pass$(openssl rand -hex 3)"
+  
+  RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X POST "$TARGET_URL" \
+    -d "username=$USER&password=$PASS" \
+    -H "Content-Type: application/x-www-form-urlencoded")
+
+  echo "[+] Sent fake login -> $USER:$PASS | HTTP $RESPONSE"
+  sleep 0.2
+done
