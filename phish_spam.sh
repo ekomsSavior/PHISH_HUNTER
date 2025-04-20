dc34f31e10d345c843707cc2335b8fd2584b61c5
ekomsSavior <161806886+ekomsSavior@users.noreply.github.com>
2025-04-20 11:33:54 -0700

    Update phish_spam.sh

--- phish_spam.sh
+++ phish_spam.sh
@@ -2,7 +2,7 @@
 # phish_spam.sh
 # by ekomsSavior
 
-TARGET_URL="http://74.98.47.117/"                  #edit for you scammers ip
+TARGET_URL="http://74.98.47.117/"                  #edit ip
 
 while true; do
   USER="user$(openssl rand -hex 3)"
